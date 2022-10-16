1b11dc9ebe8b30b382a2ab8684671e98bb201432
Snory <robtill.in@gmail.com>
2022-10-16 10:12:55 +0200

    Add project files.

---
+++ HighScore.API/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline (aka middleware)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.UseEndpoints(
+        (cosi) => cosi.MapControllers()    
+    );
+
+//app.Run(async (context) =>
+//{
+//    await context.Response.WriteAsync("Hello world");
+//});
+
+
+
+app.Run();
