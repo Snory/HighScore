dab8375a51de8a35552e7abd74aae3c6d8e64efc
Snory <robtill.in@gmail.com>
2022-10-16 10:28:46 +0200

    first working get

--- HighScore.API/Controllers/UserController.cs
+++ HighScore.API/Controllers/UserController.cs
@@ -5,10 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HighScore.API.Controllers
 {
-    [Route("api/user")]
+    [Route("api/users")]
     [ApiController]
     public class UserController : ControllerBase
     {
-
+        //default routing attribute based on router attribute defined for class
+        [HttpGet]
+        public JsonResult GetUsers()
+        {
+            return new JsonResult(
+                new List<Object>
+                {
+                    new { id = 1, Name = "Bruce Willis"},
+                    new { id = 2, Name = "Jackie Chan"}
+                }
+            );
+        }
     }
 }
--- HighScore.API/Program.cs
+++ HighScore.API/Program.cs
@@ -17,16 +17,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(
+        (endpoints) => endpoints.MapControllers()    
+    );
+
 app.MapControllers();
 
-app.UseRouting();
-
-app.UseEndpoints(
-        (cosi) => cosi.MapControllers()    
-    );
 
 //app.Run(async (context) =>
 //{
