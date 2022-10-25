a7118056c8ab5e51cf854163a9de34a6938cc450
Snory <robtill.in@gmail.com>
2022-10-25 07:53:27 +0200

    add entityframework

---
+++ HighScore.Data/Context/HighScoreContext.cs
@@ -0,0 +1,21 @@
+﻿using HighScore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighScore.Data.Context
+{
+    public class HighScoreContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<HighScore.Domain.Entities.HighScore> HighScores { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data source = sensei\\konoha; Initial Catalog=HighScore; Integrated Security=True;"); //inject it from settings later
+        }
+    }
+}
