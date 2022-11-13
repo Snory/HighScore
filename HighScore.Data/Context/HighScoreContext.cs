ab29098c9bde93d79c0fcaef0b927d28d727a9d9
Snory <robtill.in@gmail.com>
2022-11-13 14:44:58 +0100

    add comment

--- HighScore.Data/Context/HighScoreContext.cs
+++ HighScore.Data/Context/HighScoreContext.cs
@@ -10,6 +10,7 @@ namespace HighScore.Data.Context
         public DbSet<HighScoreEntity> HighScores { get; set; } = null!;
 
         //works but i have to run the migratons with more complicated cmdlet + add dependency to .API for EF
+        //with factory EF can be run from package manager, but boy, i am not sure how to inject the context
         //public HighScoreContext(DbContextOptions<HighScoreContext> options) : base(options)
         //{
 
